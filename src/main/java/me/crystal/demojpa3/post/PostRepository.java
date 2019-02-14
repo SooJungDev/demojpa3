650d3cd4d61c1316bfd7399d97445dc9d12b7af1
crystal <crystal2840@gmail.com>
2019-02-14 17:38:42 +0900

    :fire: querydsl추가

--- src/main/java/me/crystal/demojpa3/post/PostRepository.java
+++ src/main/java/me/crystal/demojpa3/post/PostRepository.java
@@ -1,6 +1,7 @@
 package me.crystal.demojpa3.post;
 
 import me.crystal.demojpa3.MyRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PostRepository extends MyRepository<Post, Long> {
+public interface PostRepository extends MyRepository<Post, Long> , QuerydslPredicateExecutor<Post> {
 }
--- src/test/java/me/crystal/demojpa3/post/PostRepositoryTest.java
+++ src/test/java/me/crystal/demojpa3/post/PostRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.crystal.demojpa3.post;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,32 +22,16 @@ public class PostRepositoryTest {
     @Autowired
     PostRepository postRepository;
 
- /*   @Autowired
-    ApplicationContext applicationContext;
-
-    @Test
-    public void event() {
-        Post post = new Post();
-        post.setTitle("event");
-        PostPublishedEvent event = new PostPublishedEvent(post);
-        applicationContext.publishEvent(event);
-    }*/
 
     @Test
     public void crud() {
         Post post = new Post();
         post.setTitle("hibernate");
-
-        //Transient 상태
-        assertThat(postRepository.contains(post)).isFalse();
-
         postRepository.save(post.publish());
 
-        //persistent 상태
-        assertThat(postRepository.contains(post)).isTrue();
-
-        postRepository.delete(post);
-        postRepository.flush();
+        Predicate predicate=QPost.post.title.containsIgnoreCase("Hi");
+        Optional<Post> one = postRepository.findOne(predicate);
+        assertThat(one).isNotEmpty();
     }
 
 }
